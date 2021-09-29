c231452cb032ebec5ce48962dc3cb0f3d929bf27
Yakovlev Kll <yakovlev.kll@gmail.com>
2021-09-29 09:15:32 +0000

    Client: list with cities updated

--- client/src/services/api/schedulePages.ts
+++ client/src/services/api/schedulePages.ts
@@ -1,16 +1,17 @@
 import { ISchedulePages } from './typings'
 
 const pages: ISchedulePages = {
-  moscow: { region: 'Europe', city: 'Moscow', time: 'GMT+3', emoji: '' },
-  almaty: { region: 'Asia', city: 'Almaty', time: 'GMT+6', emoji: '' },
-  dubai: { region: 'Asia', city: 'Dubai', time: 'GMT+4', emoji: '' },
+  moscow: { region: 'Europe', city: 'Moscow', time: 'GMT+3', emoji: '🇷🇺' },
+  almaty: { region: 'Asia', city: 'Almaty', time: 'GMT+6', emoji: '🇰🇿' },
+  dubai: { region: 'Asia', city: 'Dubai', time: 'GMT+4', emoji: '🇦🇪' },
   barcelona: {
     region: 'Europe',
     city: 'Barcelona',
     time: 'GMT+2',
-    emoji: '',
+    emoji: '🇪🇸',
   },
-  tallinn: { region: 'Europe', city: 'Tallinn', time: 'GMT+2', emoji: '' },
+  tallinn: { region: 'Europe', city: 'Tallinn', time: 'GMT+2', emoji: '🇪🇪' },
+  toronto: { region: 'America', city: 'Toronto', time: 'GMT-4', emoji: '🇨🇦' },
 }
 
 export default pages
