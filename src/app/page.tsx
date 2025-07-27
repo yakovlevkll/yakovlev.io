c82af36483bd385d9fe421e88a49fa403f9db558
Yakovlev Kirill <yakovlev.kll@gmail.com>
2025-07-27 19:24:24 +0000

    fix: :lipstick: UI improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ function Newsletter() {
       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
     >
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-        <MailIcon className="h-6 w-6 flex-none" />
+        <MailIcon className="h-6 w-6 flex-none fill-zinc-500" />
         <span className="ml-3">Stay up to date</span>
       </h2>
       <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
@@ -82,7 +82,12 @@ function Role({ role }: { role: Role }) {
     <li className="flex gap-4">
       <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
         {/* <Image src={role.logo} alt="" className="h-7 w-7" unoptimized /> */}
-        <Img src={role.logo} alt="" className="h-7 w-7" unoptimized />
+        <Img
+          src={role.logo}
+          alt=""
+          className="h-7 w-7 rounded-full"
+          unoptimized
+        />
       </div>
       <dl className="flex flex-auto flex-wrap gap-x-2">
         <dt className="sr-only">Company</dt>
