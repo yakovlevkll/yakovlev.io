57dea2791c657366b64955ee5f410c469be0fd90
Yakovlev Kirill <yakovlev.kll@gmail.com>
2025-07-27 19:18:35 +0000

    fix: :lipstick: Top avatar reverted

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -158,7 +158,7 @@ function AvatarContainer({
     <div
       className={clsx(
         className,
-        'h-16 w-16 rounded-md bg-white/90 p-0.5 shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:ring-white/10',
+        'h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:ring-white/10',
       )}
       {...props}
     />
@@ -184,7 +184,7 @@ function Avatar({
         alt=""
         sizes={large ? '4rem' : '2.25rem'}
         className={clsx(
-          'bg-zinc-100 object-cover dark:bg-zinc-800',
+          'rounded-full bg-zinc-100 object-cover dark:bg-zinc-800',
           large ? 'h-16 w-16' : 'h-9 w-9',
         )}
         priority
